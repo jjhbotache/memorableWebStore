daa89c33d43619247fe514d9b9eea992fc41d2be
S4RMI618 <huessabe@gmail.com>
2023-07-12 13:49:34 -0500

    added folders of styles, assets/fonts and edit of landingPage

--- src/pages/landingPage/landingPage.jsx
+++ src/pages/landingPage/landingPage.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './landingPage.css';
+import '../../styles/layout.css';
+import '../../styles/config.css';
 
 
 export default function LandingPage() {
   return (
     <main className="main-lp d-flex justify-content-center align-content-center flex-column text-center">
-      <h1 >MEMORABLE</h1>
-      <h2>Best wines that you would taste</h2>
-      <a className="btn mx-auto a-lp" href='/catalog'>watch designs</a>
+      <h1 className='title-lp title'>MEMORABLE</h1>
+      <h2 className='capitalize mb-3'>Best wines that you would taste</h2>
+      <a className="btn mx-auto a-lp" href='/catalog'>Watch Designs</a>
     </main>
   );
 };
