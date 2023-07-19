7904ed7496ebb65755ab93a41b597a81b6b7d2c9
S4RMI618 <huessabe@gmail.com>
2023-07-19 09:47:59 -0500

    loginForm

--- src/components/loginForm/loginForm.jsx
+++ src/components/loginForm/loginForm.jsx
@@ -12,7 +12,7 @@ export default function LoginForm({ changeForm, submitedFormFunction }) {
           />
         </div>
 
-        <div className="col-12 mt-4">
+        <div className="col-12 mt-3">
           <input
             className="form-control"
             type="number"
@@ -20,12 +20,14 @@ export default function LoginForm({ changeForm, submitedFormFunction }) {
             placeholder="Phone number"
           />
         </div>
-          <button className="btn" type="submit">
-            login
+        <div className="col-12 d-flex justify-content-center mt-4">
+          <button className="btn py-0" type="submit">
+            Enter
           </button>
+        </div>
       </form>
-      <div className="col-12 d-flex justify-align-content-between">
-        <button className="" onClick={changeForm}>
+      <div className="col-12 d-flex justify-content-center mt-2"> 
+        <button className="btn py-0" onClick={changeForm}>
           Not registered yet?
         </button>
       </div>
